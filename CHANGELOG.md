98afa84c6a6cfed917915c6dacdd60209d2525c7
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2025-05-30 19:16:42 -0500

    feat(dateUtils): add local abbreviation option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @zl-asica/react
 
+## 0.7.4
+
+### Patch Changes
+
+- Add local abbreviation option
+
 ## 0.7.3
 
 ### Patch Changes
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -5,6 +5,7 @@
  * - `YYYY`: Four-digit year (e.g., 2024).
  * - `YY`: Two-digit year (e.g., 24).
  * - `MM`: Two-digit month (e.g., 01 for January).
+ * - `MMM`: Abbreviated month name (e.g., Jan, Feb).
  * - `DD`: Two-digit day of the month (e.g., 09).
  * - `HH`: Two-digit hour in 24-hour format (e.g., 15 for 3 PM).
  * - `mm`: Two-digit minutes (e.g., 07).
@@ -15,12 +16,13 @@
  * @returns {string} A formatted date string. Returns an empty string if the input is invalid.
  *
  * @example
- * ```tsx
- * const now = new Date('2024-11-18T12:34:56Z');
+ * ```ts
+ * const now = new Date('2024-02-18T12:34:56Z');
  *
- * formatDate(now, 'YYYY-MM-DD'); // '2024-11-18'
+ * formatDate(now, 'YYYY-MM-DD'); // '2024-02-18'
+ * formatDate(now, 'MMM DD, YYYY'); // 'Feb 18, 2024'
  * formatDate(now, 'YYYY-MM-DD HH:mm:ss'); // '2024-11-18 12:34:56'
- * formatDate(now, 'MM/DD/YYYY'); // '11/18/2024'
+ * formatDate(now, 'MM/DD/YYYY'); // '02/18/2024'
  * ```
  */
 export const formatDate = (date?: Date | string | null, format = 'YYYY-MM-DD'): string => {
@@ -41,13 +43,14 @@ export const formatDate = (date?: Date | string | null, format = 'YYYY-MM-DD'): 
     YYYY: `${date.getFullYear()}`,
     YY: `${date.getFullYear()}`.slice(-2),
     MM: `${date.getMonth() + 1}`.padStart(2, '0'),
+    MMM: date.toLocaleString('default', { month: 'short' }), // e.g., 'Feb'
     DD: `${date.getDate()}`.padStart(2, '0'),
     HH: `${date.getHours()}`.padStart(2, '0'),
     mm: `${date.getMinutes()}`.padStart(2, '0'),
     ss: `${date.getSeconds()}`.padStart(2, '0'),
   }
 
-  return format.replaceAll(/YYYY|YY|MM|DD|HH|mm|ss/g, matched => map[matched])
+  return format.replaceAll(/YYYY|YY|MMM|MM|DD|HH|mm|ss/g, matched => map[matched])
 }
 
 /**
